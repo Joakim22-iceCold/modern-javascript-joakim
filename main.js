f79c60e6f092bd2b912c8d25b37b2324d79c1f14
Joakim Bøe-Sørbo <joaki.b.sorbo@gmail.com>
2025-11-11 12:36:38 +0100

    ferdig oppg1

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("Hello World!")
+console.log("Programmering er vanskelig")
+console.log("String concatenation betyr å slå sammen (kombinere) to eller flere tekststrenger (“strings”) til én lengre tekststreng.")
+console.log(16+3+79+"fødselsdato")
