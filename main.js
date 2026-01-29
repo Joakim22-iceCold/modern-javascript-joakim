a1f05ea6bbc14838b51b88b6c02ac7d5349902eb
Joakim Bøe-Sørbo <joaki.b.sorbo@gmail.com>
2026-01-29 09:40:53 +0100

    Gjort hele oppgavesett-10

--- main.js
+++ main.js
@@ -1,143 +1,145 @@
-// Joakim Bøe-Sørbo Oppgavesett 9 introduksjon oop literal objects
+// Joakim Bøe-Sørbo Oppgavesett 10 callback funksjoner
 
 // Oppgave 1
-const elev1 = {
-    fornavn: "Mikael",
-    etternavn: "Feldt",
-    alder: 16
-};
+// a)
+function helloWorld() {
+  console.log("Hello World");
+}
 
-const elev2 = {
-    fornavn: "Kim",
-    etternavn: "Tran",
-    alder: 67
-};
+function imAlive() {
+  console.log("I'm alive!");
+}
 
-const elev3 = {
-    fornavn: "Sander",
-    etternavn: "Tennstrand",
-    alder: 16
-};
+function greenLife() {
+  console.log("Its not easy being green!");
+}
 
-const elev4 = {
-    fornavn: "Jonatan",
-    etternavn: "Brekke",
-    alder: 16
-};
+helloWorld();
+imAlive();
+greenLife();
 
-console.log(elev1.fornavn, elev1.etternavn, elev1.alder);
-console.log(elev2.fornavn, elev2.etternavn, elev2.alder);
-console.log(elev3.fornavn, elev3.etternavn, elev3.alder);
-console.log(elev4.fornavn, elev4.etternavn, elev4.alder);
+
+// b)
+function executor(fn) {
+  fn();
+}
+
+executor(() => console.log("Amors piler treffer alltid i hjertet!"));
+executor(() => console.log("Cri’ Cri’ d’amore!"));
+executor(() => console.log("Parles vous francois?"));
 
 
 // Oppgave 2
-function skrivUtElev(elev) {
-    console.log(
-        "Navn: " + elev.fornavn +
-        " Etternavn: " + elev.etternavn +
-        " Alder: " + elev.alder
-    );
+// a)
+function sayHello(name) {
+  console.log(`Hello ${name}`);
 }
 
-skrivUtElev(elev1);
-skrivUtElev(elev2);
-skrivUtElev(elev3);
-skrivUtElev(elev4);
+function loggedIn(name) {
+  console.log(`${name} just logged in`);
+}
+
+function loggedOut(name) {
+  console.log(`${name} just logged out`);
+}
+
+sayHello("Kim Tran");
+loggedIn("Svein Are Brekke");
+loggedOut("Jonatan Sønderland Brekke");
+
+
+// b)
+function executor(fn) {
+  fn();
+}
+
+executor(() => console.log("Franskmenn liker ikke piler"));
+
+executor(() => console.log("Piler kan være farlige, eller de kan bare vise vei!"));
+
+executor(() => console.log("Pilgård er ikke en pil, selv om han piler fra sted til sted!"));
 
 
 // Oppgave 3
-/*
-function carFactory(seter, hestekrefter, modell, type) {
-    return {
-        seter: seter,
-        hestekrefter: hestekrefter,
-        modell: modell,
-        type: type
-    };
+
+function runRoutine(...routines) {
+  let index = 0;
+  let lastTime = Date.now();
+
+  function runNext() {
+    if (index >= routines.length) return;
+
+    const delay = Math.random() * 2000;
+
+    setTimeout(() => {
+      const now = Date.now();
+      console.log(
+        `Tid siden forrige kall: ${((now - lastTime) / 1000).toFixed(2)} sek`
+      );
+
+      routines[index]();
+      lastTime = now;
+      index++;
+      runNext();
+    }, delay);
+  }
+
+function taskA() {
+  console.log("Oppgave A");
 }
 
-const bil1 = carFactory(5, 150, "Toyota Corolla", "Sedan");
-const bil2 = carFactory(7, 200, "Volvo XC90", "SUV");
-const bil3 = carFactory(5, 180, "Volkswagen Passat", "Stasjonsvogn");
-const bil4 = carFactory(2, 300, "Mazda MX-5", "Sport");
-
-const biler = [bil1, bil2, bil3, bil4];
-*/
-
-
-// Oppgave 4
-//function carFactory(seter, hestekrefter, modell, type) {
-    //return {
-        //seter: seter,
-        //hestekrefter: hestekrefter,
-        //modell: modell,
-        //type: type,
-
-        //genererSerienummer: function () {
-            //return Math.floor(Math.random() * 1000000);
-        //}
-    //};
-//}
-
-//const bil1 = carFactory(5, 150, "Toyota Corolla", "Sedan");
-//bil1.serienummer = bil1.genererSerienummer();
-
-//const bil2 = carFactory(7, 200, "Volvo XC90", "SUV");
-//bil2.serienummer = bil2.genererSerienummer();
-
-//const bil3 = carFactory(5, 180, "Volkswagen Passat", "Stasjonsvogn");
-//bil3.serienummer = bil3.genererSerienummer();
-
-//const bil4 = carFactory(2, 300, "Mazda MX-5", "Sport");
-//bil4.serienummer = bil4.genererSerienummer();
-
-
-
-//const biler = [bil1, bil2, bil3, bil4];
-
-//biler.forEach(bil => {
-    //console.log(
-        //"Serienummer: " + bil.serienummer +
-        //", Modell: " + bil.modell +
-        //", Type: " + bil.type +
-        //", Seter: " + bil.seter +
-        //", Hestekrefter: " + bil.hestekrefter
-    //);
-//});
-
-
-// Oppgave 5
-function lagBilFactory(merke) {
-    return function (seter, hestekrefter, modell, type) {
-        return {
-            merke: merke,
-            seter: seter,
-            hestekrefter: hestekrefter,
-            modell: modell,
-            type: type
-        };
-    };
+function taskB() {
+  console.log("Oppgave B");
 }
 
+function taskC() {
+  console.log("Oppgave C");
+}
 
-const fordFactory = lagBilFactory("Ford");
+const tasks = [taskA, taskB, taskC];
+const routineCalls = [];
 
+for (let i = 0; i < 10; i++) {
+  routineCalls.push(tasks[i % tasks.length]);
+}
 
-const bil1 = fordFactory(5, 150, "Focus", "Sedan");
-const bil2 = fordFactory(7, 220, "Explorer", "SUV");
-const bil3 = fordFactory(2, 300, "Mustang", "Sport");
+runRoutine(...routineCalls)};
 
+function runRoutine(...routines) {
+  let index = 0;
+  let lastTime = Date.now();
 
-const biler = [bil1, bil2, bil3];
+  function next() {
+    if (index >= routines.length) return;
 
+    const delay = Math.random() * 2000;
 
-biler.forEach(bil => {
-    console.log(
-        "Merke: " + bil.merke +
-        ", Modell: " + bil.modell +
-        ", Type: " + bil.type +
-        ", Seter: " + bil.seter +
-        ", Hestekrefter: " + bil.hestekrefter
-    );
-});
+    setTimeout(() => {
+      const now = Date.now();
+      console.log(
+        `Tid siden forrige kall: ${((now - lastTime) / 1000).toFixed(2)} sek`
+      );
+
+      routines[index]();
+      lastTime = now;
+      index++;
+      next();
+    }, delay);
+  }
+
+  next();
+}
+
+const functions = [
+  () => console.log("Funksjon 1"),
+  () => console.log("Funksjon 2"),
+  () => console.log("Funksjon 3")
+];
+
+const calls = [];
+
+for (let i = 0; i < 10; i++) {
+  calls.push(functions[i % functions.length]);
+}
+
+runRoutine(...calls);
